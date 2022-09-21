0702575b530000cd0e6472897ba2d87497041522
sagar <sagarbehera156@gmail.com>
2022-09-21 21:43:28 +0530

    Attendance

---
+++ Empcheck.java
@@ -0,0 +1,17 @@
+package problem;
+
+public class Empcheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int Fulltime =1;
+	      double empcheck =Math.floor(Math.random()*10)%2;
+	      if (empcheck==Fulltime) {
+	    	  System.out.println("Employee is present");
+	      }
+	    	  else {
+	    		  System.out.println("Employee is apscent");
+	    	  }
+	}
+
+}
